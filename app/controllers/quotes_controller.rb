61b5a22014225cc2fcf903501f4d3d6732bbeafb
KD33 <Walshk94@gmail.com>
2018-02-19 20:24:58 +0000

    Quotes added to database

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,6 @@
 class QuotesController < ApplicationController
   def index
+    #store a random quote from our database in a variable called @quote
+    @quote = Quote.order("RANDOM()").first
   end
 end
