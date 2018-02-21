48088c7b3d26a8d70a43ffa62a8ef2f81b48aa19
KD33 <Walshk94@gmail.com>
2018-02-21 05:39:14 +0000

    store quote in db from form input

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,4 +8,19 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
 
+  def create
+    #This is what sends the saying and author to the database so they are saved
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    #this pulls the values of saying and author from the quotes
+    #permit is a safety measure to make sure hackers cannot inject anything
+    #other than saying and author into the quote form
+    params.require(:quote).permit(:saying, :author)
+  end
+
 end
