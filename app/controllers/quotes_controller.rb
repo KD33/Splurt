8699d9760acf55adfd5002023a88216726e37523
KD33 <Walshk94@gmail.com>
2018-02-21 05:16:01 +0000

    Beginning of quote form implementation

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,4 +3,9 @@ class QuotesController < ApplicationController
     #store a random quote from our database in a variable called @quote
     @quote = Quote.order("RANDOM()").first
   end
+
+  def new
+    @quote = Quote.new
+  end
+
 end
